959f14919b68cec87e59755511528d5f433c8345
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-01-03 16:44:53 +0800

    Add SWR

--- components/Books.jsx
+++ components/Books.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Parser from "rss-parser";
 import NextLink from "./NextLink";
 import {
@@ -12,23 +13,38 @@ import {
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Books = () => {
-  const [feed, setFeed] = useState({ title: "", items: [] });
-  const rssData = async () => {
-    // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-    let parser = new Parser();
+  // const [feed, setFeed] = useState({ title: "", items: [] });
+  // const rssData = async () => {
+  //   // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+  //   let parser = new Parser();
 
-    try {
-      const feed = await parser.parseURL(
-        `https://damp-lowlands-80262.herokuapp.com/oku`
-      );
-      setFeed(feed);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    rssData();
-  }, []);
+  //   try {
+  //     const feed = await parser.parseURL(
+  //       `https://damp-lowlands-80262.herokuapp.com/oku`
+  //     );
+  //     setFeed(feed);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   rssData();
+  // }, []);
+
+  function fetcher(url) {
+    let parser = new Parser();
+    const result = parser.parseURL(url);
+    console.log(result);
+    return result;
+  }
+
+  const { data, error } = useSWR(
+    "https://damp-lowlands-80262.herokuapp.com/oku",
+    fetcher
+  );
+
+  // if (error) return "An error has occurred.";
+  // if (!data) return "Loading...";
 
   return (
     <>
@@ -41,27 +57,32 @@ const Books = () => {
         <Text m="0" mb="2">
           📚 最近在讀
         </Text>
-        {feed.items.slice(0, 5).map((item, i) => (
-          <Box key={i} mb="4">
-            <NextLink
-              fontSize="lg"
-              fontWeight="bold"
-              lineHeight="0"
-              href={item.link}
-              title={`Read more about ${item.title} on Oku`}
-              target="_blank"
-              isExternal
-            >
-              <HStack>
-                <Text>{item.title}</Text> <FaExternalLinkAlt />
-              </HStack>
-            </NextLink>
 
-            <Text m="0" fontSize="xs" textTransform="uppercase">
-              by {item.creator}
-            </Text>
-          </Box>
-        ))}
+        {!data ? (
+          <p>Loading </p>
+        ) : (
+          data.items.slice(0, 5).map((item, i) => (
+            <Box key={i} mb="4">
+              <NextLink
+                fontSize="lg"
+                fontWeight="bold"
+                lineHeight="0"
+                href={item.link}
+                title={`Read more about ${item.title} on Oku`}
+                target="_blank"
+                isExternal
+              >
+                <HStack>
+                  <Text>{item.title}</Text> <FaExternalLinkAlt />
+                </HStack>
+              </NextLink>
+
+              <Text m="0" fontSize="xs" textTransform="uppercase">
+                by {item.creator}
+              </Text>
+            </Box>
+          ))
+        )}
       </Box>
     </>
   );
--- components/Films.jsx
+++ components/Films.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Parser from "rss-parser";
 import NextLink from "./NextLink";
 import { Text, Grid, Tooltip, Box, useColorModeValue } from "@chakra-ui/react";
 import Image from "next/image";
 
 const Films = () => {
-  const [feed, setFeed] = useState({ title: "", items: [] });
+  // const [feed, setFeed] = useState({ title: "", items: [] });
 
   /* ImageParser for Letterboxd Film Poster */
   function imageParser(htmlString) {
@@ -17,22 +18,37 @@ const Films = () => {
     return imgLink;
   }
 
-  const rssData = async () => {
-    // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-    let parser = new Parser();
+  // const rssData = async () => {
+  //   // const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+  //   let parser = new Parser();
 
-    try {
-      const feed = await parser.parseURL(
-        `https://damp-lowlands-80262.herokuapp.com/letterboxd`
-      );
-      setFeed(feed);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    rssData();
-  }, []);
+  //   try {
+  //     const feed = await parser.parseURL(
+  //       `https://damp-lowlands-80262.herokuapp.com/letterboxd`
+  //     );
+  //     setFeed(feed);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   rssData();
+  // }, []);
+
+  function fetcher(url) {
+    let parser = new Parser();
+    const result = parser.parseURL(url);
+    console.log(result);
+    return result;
+  }
+
+  const { data, error } = useSWR(
+    "https://damp-lowlands-80262.herokuapp.com/letterboxd",
+    fetcher
+  );
+
+  // if (error) return "An error has occurred.";
+  // if (!data) return "Loading...";
 
   return (
     <>
@@ -41,35 +57,81 @@ const Films = () => {
         p="4"
         borderRadius="16"
         gridColumn="span 2"
+        width="100%"
       >
         <Text m="0" mb="2">
           🎬 最近在看
         </Text>
         <Grid gridTemplateColumns="repeat(5, 1fr)" transform="scale(0.9)">
-          {feed.items.slice(0, 5).map((item, i) => (
-            <Box
-              key={i}
-              mr="-30px"
-              zIndex={i}
-              transition="all 100ms ease-in-out"
-              _hover={{
-                zIndex: "100",
-                transform: "rotate3d(1, 1, 1,2deg) scale3d(1.1, 1.1, 1.1)",
-              }}
-              boxShadow="2px 0 7px grey;"
-            >
-              <NextLink href={item.link} target="_blank">
-                <Tooltip label={item.title} fontSize="md" mt="10px">
-                  <img
-                    src={imageParser(item.content)}
-                    width="200"
-                    height="300"
-                    alt={item.title}
-                  ></img>
-                </Tooltip>
-              </NextLink>
-            </Box>
-          ))}
+          {!data ? (
+            <>
+              <Box mr="-30px" boxShadow="2px 0 7px grey;">
+                <Box
+                  backgroundColor="black"
+                  width="200"
+                  height="300"
+                  alt="Placeholder Image"
+                ></Box>
+              </Box>{" "}
+              <Box mr="-30px" boxShadow="2px 0 7px grey;">
+                <Box
+                  backgroundColor="black"
+                  width="200"
+                  height="300"
+                  alt="Placeholder Image"
+                ></Box>
+              </Box>{" "}
+              <Box mr="-30px" boxShadow="2px 0 7px grey;">
+                <Box
+                  backgroundColor="black"
+                  width="200"
+                  height="300"
+                  alt="Placeholder Image"
+                ></Box>
+              </Box>{" "}
+              <Box mr="-30px" boxShadow="2px 0 7px grey;">
+                <Box
+                  backgroundColor="black"
+                  width="200"
+                  height="300"
+                  alt="Placeholder Image"
+                ></Box>
+              </Box>
+              <Box mr="-30px" boxShadow="2px 0 7px grey;">
+                <Box
+                  backgroundColor="black"
+                  width="200"
+                  height="300"
+                  alt="Placeholder Image"
+                ></Box>
+              </Box>
+            </>
+          ) : (
+            data.items.slice(0, 5).map((item, i) => (
+              <Box
+                key={i}
+                mr="-30px"
+                zIndex={i}
+                transition="all 100ms ease-in-out"
+                _hover={{
+                  zIndex: "100",
+                  transform: "rotate3d(1, 1, 1,2deg) scale3d(1.1, 1.1, 1.1)",
+                }}
+                boxShadow="2px 0 7px grey;"
+              >
+                <NextLink href={item.link} target="_blank">
+                  <Tooltip label={item.title} fontSize="md" mt="10px">
+                    <img
+                      src={imageParser(item.content)}
+                      width="200"
+                      height="300"
+                      alt={item.title}
+                    ></img>
+                  </Tooltip>
+                </NextLink>
+              </Box>
+            ))
+          )}
         </Grid>
       </Box>
     </>
