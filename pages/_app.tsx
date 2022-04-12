9b03df9eae3facfd872417a3219671d70f49c2bf
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-04-12 11:52:35 +0800

    - add follow it verify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,6 +46,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/icons/favicon.svg" />
         <link rel="mask-icon" href="mask-icon.svg" color="#000000" />
         <link rel="apple-touch-icon" href="/icons/favicon.png" />
+        <meta
+          name="follow.it-verification-code"
+          content="MW6ljDEjp6gmF4pZDB4U"
+        />
       </Head>
       <NextNProgress height={4} color="#0077CC" />
       <ChakraProvider resetCSS={true} theme={customTheme}>
